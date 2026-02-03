7d42c4b1664bb8859ff6489d5b03d61ccce59129
JamesCowley10 <22256jc@hvhs.school.nz>
2026-02-04 12:05:58 +1300

    Test

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 var messageSpace = document.getElementById("welcomeMessage");
 messageSpace.innerHTML = "You've connected to the JavaScript!";
+
+var textInput = document.getElementById("textInput");
+
+function onTextInputButtonClick() {
+    messageSpace.innerHTML = textInput.value;
+}
