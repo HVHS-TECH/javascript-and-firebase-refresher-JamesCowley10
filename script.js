1c199f5432b028ba6f2c35625944d4d0ba25b00b
JamesCowley10 <22256jc@hvhs.school.nz>
2026-02-04 12:28:35 +1300

    Push 1

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ var messageSpace = document.getElementById("welcomeMessage");
 messageSpace.innerHTML = "You've connected to the JavaScript";
 
 var textInput = document.getElementById("textInput");
+var textInputFirebase = document.getElementById("textInputFirebase");
 
 function onTextInputButtonClick() {
     messageSpace.innerHTML = textInput.value;
+}
+
+function onTextInputFirebaseButtonClick() {
+    //send to firebase
 }
